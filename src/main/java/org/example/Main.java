03c8118b534c7b7387bb76c0b2edeb860eb29cd1
JoaovBenjamin <joaovvbenjamin@hotmail.com>
2026-03-16 22:01:59 -0300

    Aprendendo o básico de seletores no Selenium, XPath, CssSelector, Class, Name e Id. Aprendendo também o básico de comportamento de elementos da página

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
 
         WebDriver driver = new ChromeDriver();
+        // Esse Drive Manage posso configurar o Manager do Seleniu e alterar suas configs
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
         driver.findElement(By.id("inputUsername")).sendKeys("hello world");
         driver.findElement(By.name("inputPassword")).sendKeys("teste 123");
         driver.findElement(By.className("submit")).click();
-
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("JoaoVVB");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("joaovitorvicentebenjamin@gmail.com");
+        driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("123");
+        // driver.findElement(By.xpath("(//input[@placeholder='Email'])[1]")).clear();
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        System.out.println(driver.findElement(By.cssSelector(".infoMsg")).getText());
     }
 }
